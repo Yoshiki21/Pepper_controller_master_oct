033cc3cb5206a8e178dbaf628e87a6e54c2a9996
Yoshiki21 <yoshinak21@gmail.com>
2021-10-15 15:34:30 +0900

    望さんようのファイルを追加

---
+++ js/control_diagonal_nozomu.js
@@ -0,0 +1,431 @@
+var self = this;
+
+function qrCreate(){
+
+	var ipAddress = $('#socketServerIP').val();
+	var portNumber = $('#socketServerPort').val();
+
+	var jsonText = 	'{"ip":"' +
+									ipAddress +
+									'","port":' +
+									portNumber +
+									'}'
+	console.log(jsonText)
+	$('#qrcodeCanvas').empty();
+
+
+	$('#qrcodeCanvas').qrcode({
+		text	: jsonText
+	});
+}
+
+function connect(){
+	// 入力されたpepperのIPアドレスを取得
+	var pepperIp = $("#pepperIP").val();
+
+	// 接続が成功したら、各種プロキシを作成して代入しておく
+    var setupIns_ = function(){
+    	self.qims.service("ALTextToSpeech").done(function(ins){
+    		self.alTextToSpeech = ins;
+        });
+        self.qims.service("ALAnimatedSpeech").done(function(ins){
+    		self.alAnimatedSpeech = ins;
+        });
+        self.qims.service("ALMotion").done(function(ins){
+        	self.alMotion = ins;
+        });
+        self.qims.service("ALBehaviorManager").done(function(ins){
+        	self.alBehavior = ins;
+        });
+    	self.qims.service("ALAutonomousLife").done(function(ins){
+    		self.alAutonomousLife = ins;
+        });
+        self.qims.service("ALAudioDevice").done(function(ins){
+            self.alAudioDevice = ins;
+            self.alAudioDevice.getOutputVolume().done(function(val){
+		    self.showAudioVolume(val);
+		    });
+        });
+        self.qims.service("ALMemory").done(function(ins){
+    		self.alMemory = ins;
+			qimessagingCurrentSentenceSubscribe();
+    		// メモリ監視
+    		qimessagingMemorySubscribe();
+			
+        });
+    }
+
+	// pepperへの接続を開始する
+	self.qims = new QiSession(pepperIp);
+	self.qims.socket()
+		// 接続成功したら
+		.on('connect', function ()
+			{
+   	 		self.qims.service("ALTextToSpeech")
+   	 			.done(function (tts)
+   	 			{
+   	 	        	tts.say("");
+   	 	       });
+   	 	       		// 接続成功したら各種セットアップを行う
+     	       		setupIns_();
+
+     	       		// 接続成功表示切り替え
+     	       		$(".connectedState > .connected > .connectedText").text("接続成功");
+     	       		$(".connectedState > .connected > .glyphicon").removeClass("glyphicon-remove");
+     	       		$(".connectedState > .connected > .glyphicon").addClass("glyphicon-signal");
+     	       		$(".connectedState > .connected").css("color","Blue");
+
+
+     	       })
+     	// 接続失敗したら
+        .on('disconnect', function () {
+              //self.nowState("切断");
+});
+}
+
+
+function showAudioVolume(val){
+	console.log(val);
+	// あとからページに表示させる
+	$("#pepperVolume").val(val);
+}
+
+function changeAudioVolume(){
+	var volume = $("#pepperVolume").val();
+	volume = Number(volume);
+	console.log(Number(volume));
+	self.alAudioDevice.setOutputVolume(volume);
+	self.hello();
+
+}
+
+
+// 動作確認用Hello
+function hello(){
+	console.log("hello");
+	this.alTextToSpeech.say("はろー");
+
+}
+
+// おしゃべり
+function say(){
+	console.log("say");
+	var value = $("#sayText").val();
+	this.alTextToSpeech.say(value);
+}
+
+// 名前を呼ぶ関数
+function say_name(num){
+	var value = $("#name_input").val();
+	switch(num){
+		case 0:
+			this.alTextToSpeech.say(value+"三っ！");
+			break;
+		case 1:
+			this.alTextToSpeech.say(value+"三の");
+			break;
+		case 2:
+			this.alTextToSpeech.say(value+"三は");
+			break;
+	}
+	
+
+}
+
+// 動きながらおしゃべり
+function animatedSay(){
+	console.log("say");
+	var value = $("#animatedSayText").val();
+	this.alAnimatedSpeech.say(value);
+}
+
+
+
+function thank(){
+	console.log("arigato");
+	this.alAnimatedSpeech.say("ありがとう");
+}
+
+// 移動
+function move(to){
+	if (self.alMotion){
+		console.log("move to");
+		switch (to){
+			case 0:
+				self.alMotion.moveTo(0, 0, 0.5).fail(function(err){console.log(err);});
+				break;
+
+			case 1:
+				self.alMotion.moveTo(0, 0, -0.5).fail(function(err){console.log(err);});
+				break;
+
+			case 2:
+				self.alMotion.moveTo(0.3, 0, 0).fail(function(err){console.log(err);});
+				break;
+
+			case 3:
+				self.alMotion.moveTo(-0.3, 0, 0).fail(function(err){console.log(err);});
+				break;
+			case 4:
+				self.alMotion.moveTo(0, 0, 0).fail(function(err){console.log(err);});
+				break;
+
+		}
+	}
+}
+
+// ビヘイビアアクション
+function eye_track(num){
+	switch (num){
+		case 0:
+			self.alBehavior.stopAllBehaviors();
+			break;
+		case 1:
+			self.alBehavior.runBehavior("yoshiki_master_oct/00_posture/03_eye_contact");
+			break;		
+	}
+}
+
+function aiduchi(num){
+	self.alAudioDevice.setOutputVolume(50);
+	switch (num){
+		case 0:
+			if (Math.random()>0.5){
+				self.alBehavior.runBehavior("yoshiki_master_oct/02_motion/00_nodding_diagonal")
+			}else{
+				self.alAudioDevice.setOutputVolume(40);
+				self.alBehavior.runBehavior("yoshiki_master_oct/01_aiduchi/00_un")
+			};
+			break;
+		case 1:
+			self.alBehavior.runBehavior("yoshiki_master_oct/01_aiduchi/01_naruhodo");
+			break;	
+		case 2:
+			self.alBehavior.runBehavior("yoshiki_master_oct/01_aiduchi/02_sounandesune");
+			break;	
+		case 3:
+			self.alBehavior.runBehavior("yoshiki_master_oct//01_aiduchi/03_naruhodo_sounandesune");
+			break;	
+		case 4:
+			self.alBehavior.runBehavior("yoshiki_master_oct/01_aiduchi/04_a_naruhodo");
+			break;
+		case 5:
+			self.alBehavior.runBehavior("yoshiki_master_oct/01_aiduchi/05_arigatogozaimasu");
+			break;
+		case 6:
+			self.alBehavior.runBehavior("yoshiki_master_oct/01_aiduchi/06_a_hai");
+			break;
+		case 7:
+			self.alBehavior.runBehavior("yoshiki_master_oct/01_aiduchi/07_shiranakkatadesu");
+			break;
+		case 8:
+			self.alBehavior.runBehavior("yoshiki_master_oct/01_aiduchi/08_sutekidesune");
+			break;
+		case 9:
+			self.alBehavior.runBehavior("yoshiki_master_oct/01_aiduchi/09_sugokuiidesune");
+			break;
+		case 10:
+			self.alBehavior.runBehavior("yoshiki_master_oct/01_aiduchi/10_naruhodo_gaze_averstion");
+			break;
+		case 11:
+			self.alBehavior.runBehavior("yoshiki_master_oct/01_aiduchi/11_naruhodo_mousukoshi");
+			break;
+		case 12:
+			self.alBehavior.runBehavior("yoshiki_master_oct/01_aiduchi/12_soudattandesune");
+			break;
+		case 13:
+			self.alBehavior.runBehavior("yoshiki_master_oct/01_aiduchi/13_mousukoshi");
+			break;
+	}
+}
+function aiduchi_np(num){
+	self.alAudioDevice.setOutputVolume(50);
+	switch (num){
+		case 0:
+			self.alBehavior.runBehavior("yoshiki_master_oct/03_aiduchi_np/00_un")
+			break;
+		case 1:
+			self.alBehavior.runBehavior("yoshiki_master_oct/03_aiduchi_np/01_naruhodo");
+			break;	
+		case 2:
+			self.alBehavior.runBehavior("yoshiki_master_oct/03_aiduchi_np/02_sounandesune");
+			break;	
+		case 3:
+			self.alBehavior.runBehavior("yoshiki_master_oct//03_aiduchi_np/03_naruhodo_sounandesune");
+			break;	
+		case 4:
+			self.alBehavior.runBehavior("yoshiki_master_oct/03_aiduchi_np/04_a_naruhodo");
+			break;
+		case 5:
+			self.alBehavior.runBehavior("yoshiki_master_oct/03_aiduchi_np/05_arigatogozaimasu");
+			break;
+		case 6:
+			self.alBehavior.runBehavior("yoshiki_master_oct/03_aiduchi_np/06_a_hai");
+			break;
+		case 7:
+			self.alBehavior.runBehavior("yoshiki_master_oct/03_aiduchi_np/07_soudesune");
+			break;
+		case 8:
+			self.alBehavior.runBehavior("yoshiki_master_oct/03_aiduchi_np/08_zyaa");
+			break;
+	}
+}
+function posture(num){
+	switch (num){
+		case 0:
+			self.alBehavior.runBehavior("yoshiki_master_oct/00_posture/05_sit_2")
+			break;
+		case 1:
+			self.alBehavior.runBehavior("yoshiki_master_oct/00_posture/06_lean_sit_2");
+			break;	
+	}
+}
+function question(phrase){
+	// 手を動かして話始める
+	self.alBehavior.runBehavior("yoshiki_master_oct/00_posture/02_back_voice");
+	self.alBehavior.runBehavior("yoshiki_master_oct/02_motion/07_start_talking_positives");
+	self.alTextToSpeech.say(phrase);
+}
+function question_np(phrase){
+	// 手を動かさず話し始める
+	self.alBehavior.runBehavior("yoshiki_master_oct/00_posture/02_back_voice");
+	self.alTextToSpeech.say(phrase);
+}
+
+function question_np_move(phrase){
+	// 手を動かさず話し始める
+	self.alBehavior.runBehavior("yoshiki_master_oct/00_posture/02_back_voice");
+	self.alBehavior.runBehavior("yoshiki_master_oct/02_motion/08_start_talking_neutrals");
+	self.alTextToSpeech.say(phrase);
+}
+
+function utterance_spe(phrase){
+	self.alBehavior.runBehavior("yoshiki_master_oct/02_motion/09_moving_to_positive");
+	self.alTextToSpeech.say(phrase);
+}
+
+function utterance_spe2(phrase){
+	self.alBehavior.runBehavior("yoshiki_master_oct/02_motion/02_start_talking_diagonal_neutral");
+	self.alTextToSpeech.say(phrase);
+}
+
+function trackmode(bl){
+	var status;
+	if (bl){
+		self.alBasicAwarenessProxy.setTrackingMode("Head");
+	}else
+	{
+		self.alBasicAwarenessProxy.stopAwareness();
+	}
+}
+function autonomousSwitch(bl){
+	var status;
+	if (bl)
+	{
+		console.log("ON");
+		self.alAutonomousLife.getState().done(function(val){console.log(val)});
+		self.alAutonomousLife.setState("solitary");
+
+	}else
+	{
+		console.log("OFF");
+		self.alAutonomousLife.getState().done(function(val){console.log(val)});
+		self.alAutonomousLife.setState("disabled");
+	}
+}
+
+function name_saying(num){
+	var value = $("#name_input").val();
+	self.alBehavior.runBehavior("yoshiki_master_oct/00_posture/02_back_voice");
+	if (num<5){
+		self.alBehavior.runBehavior("yoshiki_master_oct/02_motion/07_start_talking_positives");
+	}
+	switch(num){
+		case 1:
+			self.alTextToSpeech.say("そんな素敵な" +value+"三だからこそ、お話しいただいた、体験ができたんですねッ！");
+			break;
+		case 2:
+			self.alTextToSpeech.say("謙虚なとこ炉が、" +value+"三の、良いとこ炉ですね！");
+			break;
+		case 3:
+			self.alTextToSpeech.say("人に感謝を、忘れないとこ炉が、" +value+"三の、良いとこ炉ですねッ！");
+			break;
+		case 4:
+			self.alTextToSpeech.say("それでも頑張った" +value+"三は、素晴らしいと思います");
+			break;
+		case 5:
+			self.alTextToSpeech.say(value+"三の、ご兄弟や、ご家族についても、教えて下さい！");
+			break;
+		case 6:
+			self.alTextToSpeech.say("改めまして、"+value+"三、僕の住む、大岡山へようこそ！");
+			break;
+		case 7:
+			self.alTextToSpeech.say(value+"三の、最近あった、楽しかったことも、知りたいです！");
+			break;
+		case 8:
+			self.alTextToSpeech.say("僕は寒さが苦手だから、春になったら、"+value+"三の、故郷に胃ってみたいなーッっ！");
+			break;
+	}
+}
+
+function sleepSwitch(bl){
+	var status;
+	if (bl)
+	{
+		console.log("ON");
+		self.alMotion.wakeUp();
+
+	}else
+	{
+		console.log("OFF");
+		self.alMotion.rest();
+	}
+}
+
+
+function qimessagingMemoryEvent(){
+	console.log("push!");
+	self.alMemory.raiseEvent("PepperQiMessaging/Hey", "1");
+}
+
+function qimessagingMemorySubscribe(){
+	console.log("subscriber!");
+	self.alMemory.subscriber("PepperQiMessaging/Reco").done(function(subscriber)
+		{
+            subscriber.signal.connect(toTabletHandler);
+        }
+    );
+}
+
+function qimessagingCurrentSentenceSubscribe(){
+	self.alMemory.subscriber("ALTextToSpeech/CurrentSentence").done(function(subscriber) {
+		subscriber.signal.connect(toLogHandler);
+	  });
+}
+
+function start_set(){
+	self.alAutonomousLife.getState().done(function(val){console.log(val)});
+	self.alAutonomousLife.setState("disabled");
+	sleep(10000)
+	self.alMotion.wakeUp();
+}
+
+function toTabletHandler(value) {
+        console.log("PepperQiMessaging/Recoイベント発生: " + value);
+        $(".memory").text(value);
+}
+function sleep(waitMsec) {
+	var startMsec = new Date();
+   
+	// 指定ミリ秒間だけループさせる（CPUは常にビジー状態）
+	while (new Date() - startMsec < waitMsec);
+}
+function toLogHandler(value) {
+	var value = value.replace(/、/g,'');
+	var value = value.replace(/　/g,'');
+	if (value != ""){
+		var previous = $("#dialog").val()
+		// $("#dialog").val(previous+value);
+		$("#dialog").val(previous+"\n"+ value);
+	}
+}
